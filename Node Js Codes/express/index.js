89349c365e931b89af5bd456e332ba3f5bc67be2
AsCodes21 <AsCodes21@gmail.com>
2022-02-06 14:20:22 +0530

    Using pug (template engine)

--- Node Js Codes/express/index.js
+++ Node Js Codes/express/index.js
@@ -5,7 +5,14 @@ const port = 3000;
 app.use(express.json())
 //Access static files
 app.use("/static",express.static("static"))
+//Set template engine
+app.set('view engine', 'pug')
+//Set views directory
+app.set('views','views')
 
+app.get('/pug', function (req, res) {
+    res.render('demo', { title: 'Hey', message: 'Hello there!',msg2:"Hi there again"})
+  }) 
 app.get("/", (req, res) => {
   res.send("Hi there I am Anmol Sinha!!-Home");
 });
