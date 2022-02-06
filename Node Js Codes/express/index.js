e1cf9f2b6cafb23c864715c2b1adc989cc9c766c
AsCodes21 <AsCodes21@gmail.com>
2022-02-06 18:02:00 +0530

    Successfully setup pug with 1 template

--- Node Js Codes/express/index.js
+++ Node Js Codes/express/index.js
@@ -3,29 +3,20 @@ const app = express();
 const port = 3000;
 
 app.use(express.json())
-//Access static files
-app.use("/static",express.static("static"))
-//Set template engine
-app.set('view engine', 'pug')
-//Set views directory
-app.set('views','views')
 
+app.use("/static",express.static("static"))//Access static files
+app.set('view engine', 'pug')//Set template engine
+app.set('views','views')//Set views directory
+
+
+//ENDPOINTS
 app.get('/pug', function (req, res) {
     res.render('demo', { title: 'Hey', message: 'Hello there!',msg2:"Hi there again"})
   }) 
 app.get("/", (req, res) => {
-  res.send("Hi there I am Anmol Sinha!!-Home");
+    values = {'title':'Jai pubg'}
+  res.status(200).render('index',values);
 });
-app.get("/about", (req, res) => {
-  res.send("Hi there I am Anmol Sinha!!-About");
-});
-app.post("/form",(req,res)=>{
-    console.log(req.body)
-    res.send(req.body)
-})
-app.get("/this",(req,res)=>{
-    res.status(404).send("You are not on right place!!")
-})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
